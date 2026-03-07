706d4c631fb2eb9b5bbf221f811c936084bf2ffb
Turki <turki@MacBook-Air-Turki.local>
2026-03-07 17:42:12 +0300

    Updates

--- assets/about-RMjLjnsj.js
+++
@@ -1,1 +0,0 @@
-var e;import{a}from"./auth-BZhxXvHs.js";import{n as t}from"./navigationHelper-jIkYxdjO.js";a.requireLogin(),null==(e=document.getElementById("btn-home"))||e.addEventListener("click",()=>{t.navigateWithTransition("./home.html")}),t.setupPageLoadFadeIn();
